aa67949bf8f0633740b6b10c022848cf894edf71
AhmedOsama14-7 <hamodaoss209@gmail.com>
2024-09-10 14:39:31 +0300

    mobile menu and animations

--- resources/scripts/contact.js
+++ resources/scripts/contact.js
@@ -2,7 +2,10 @@ const menu_bar_icon = document.querySelector("i")
 const menu_bar = document.querySelector(".dropdown-menu-clicked")
 
 console.log(menu_bar,menu_bar_icon)
+
 menu_bar_icon.addEventListener('click' , function (){
     menu_bar.classList.toggle("active")
     
 })
+
+
